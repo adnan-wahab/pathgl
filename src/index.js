4069c28e2cddab983de910cbd4817560ebe52048
Adnan Wahab <adnan@meetyogi.com>
2020-01-23 16:58:18 -0500

    simplify

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 let createDrawLines = require('./edges')
 let createDrawNodes = require('./nodes')
 let createCamera = require('./camera')
+let _ = require('lodash')
+let d3 = require('d3')
 
 const mat4 = require('gl-mat4')
 let createRegl = require('regl')
+let clip = (d) => {
+  return d / 4000
+}
 
-module.exports = (options) => {
-  console.log(options.canvas)
+let processKMeans = (data) => {
+    let edges = new Array(data.edges.length * 4).fill(0);
+    data.edges.forEach((edge, idx) => {
+      edges[idx*4] = clip(data.nodes[edge.source].x)
+      edges[idx*4+1] = clip(data.nodes[edge.source].y)
+      edges[idx*4+2] = clip(data.nodes[edge.target].x)
+      edges[idx*4+3] = clip(data.nodes[edge.target].y)
+    });
+    let color = _.flatten(data.edges.map((e) => {
+      let c = d3.color(data.nodes[e.source].color);
+      return [c.r /255 , c.g /255 , c.b /255];
+    }));
+
+    let fboColor = color.map((d, i) => {
+      return i / color.length
+    })
+    return {
+      position: edges,
+      color,
+      fboColor
+    }
+  }
+
+module.exports.init = (options) => {
+  options.canvas = options.canvas || function () {}
+  options.attributes = processKMeans(options.data)
   let regl = createRegl({
     canvas: options.canvas,
     extensions: ['OES_standard_derivatives']
@@ -50,4 +79,9 @@ module.exports = (options) => {
       })
     })
   })
+
+  return {
+    paint: () => {},
+    setOptions: () => {}
+  }
 }
--- src/test.js
+++ src/test.js
@@ -1,48 +1,8 @@
 
-let _ = require('lodash')
 let GraphRenderer = require('./index')
 let d3 = require('d3')
 
-let clip = (d) => {
-  return d / 4000
-}
-
-let processKMeans = (data) => {
-    let edges = new Array(data.edges.length * 4).fill(0);
-    data.edges.forEach((edge, idx) => {
-      edges[idx*4] = clip(data.nodes[edge.source].x)
-      edges[idx*4+1] = clip(data.nodes[edge.source].y)
-      edges[idx*4+2] = clip(data.nodes[edge.target].x)
-      edges[idx*4+3] = clip(data.nodes[edge.target].y)
-    });
-    let color = _.flatten(data.edges.map((e) => {
-      let c = d3.color(data.nodes[e.source].color);
-      return [c.r /255 , c.g /255 , c.b /255];
-    }));
-
-    let fboColor = color.map((d, i) => {
-      return i / color.length
-    })
-    return {
-      position: edges,
-      color,
-      fboColor,
-    }
-  }
-
-let init = (options) => {
-  return GraphRenderer({
-    attributes: processKMeans(options.data),
-    canvas: options.canvas,
-    width: options.width,
-    height: options.heigth,
-    onHover: options.onHover || function () {},
-    data: options.data
-  })
-}
-
-module.exports = init
-
+module.exports = GraphRenderer
 
 if (window.location.port == 9966) {
   let c = [
@@ -61,6 +21,6 @@ if (window.location.port == 9966) {
       canvas.height = innerHeight
       canvas.width = innerWidth
       document.body.appendChild(canvas)
-      init({ data: json, canvas: canvas })
+      GraphRenderer.init({ data: json, canvas: canvas })
     })
 }

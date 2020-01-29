f633efb9c92cc5f35eb69b77dc4f664126435863
Adnan Wahab <adnan@meetyogi.com>
2020-01-29 10:25:25 -0500

    inline shaders because webpack

--- src/index.js
+++ src/index.js
@@ -11,11 +11,125 @@ import createLine from 'regl-line'
 import createDrawLines from './edges';
 import createDrawNodes from './nodes';
 
+let BG_FS = `
+precision mediump float;
 
-import BG_FS from './bg.fs';
-import BG_VS from './bg.vs';
-import POINT_FS from './point.fs';
-import POINT_VS from './point.vs';
+uniform sampler2D texture;
+
+varying vec2 uv;
+
+void main () {
+  gl_FragColor = texture2D(texture, uv);
+}
+`;
+let BG_VS =  `
+precision mediump float;
+
+uniform mat4 projection;
+uniform mat4 model;
+uniform mat4 view;
+
+attribute vec2 position;
+
+varying vec2 uv;
+
+void main () {
+  uv = position;
+  gl_Position = projection * view * model * vec4(1.0 - 2.0 * position, 0, 1);
+}
+`;
+;
+let POINT_FS = `
+#ifdef GL_OES_standard_derivatives
+#extension GL_OES_standard_derivatives : enable
+#endif
+
+precision mediump float;
+uniform vec2 selection;
+
+varying vec4 color;
+
+void main() {
+  float r = 0.0, delta = 0.0, alpha = 1.0;
+  vec2 cxy = 2.0 * gl_PointCoord - 1.0;
+  r = dot(cxy, cxy);
+
+  #ifdef GL_OES_standard_derivatives
+    delta = fwidth(r);
+    alpha = 1.0 - smoothstep(1.0 - delta, 1.0 + delta, r);
+  #endif
+
+  gl_FragColor = vec4(color.rgb, alpha * color.a);
+}
+`;
+let POINT_VS = `
+precision mediump float;
+
+uniform sampler2D colorTex;
+uniform float colorTexRes;
+uniform sampler2D stateTex;
+uniform float stateTexRes;
+uniform float pointSize;
+uniform float pointSizeExtra;
+uniform float numPoints;
+uniform float globalState;
+uniform float isColoredByCategory;
+uniform float isColoredByValue;
+uniform float maxColor;
+uniform float numColorStates;
+uniform float scaling;
+uniform mat4 projection;
+uniform mat4 model;
+uniform mat4 view;
+
+attribute float stateIndex;
+attribute vec2 pos;
+
+// variables to send to the fragment shader
+varying vec4 color;
+
+void main() {
+  // First get the state
+  float eps = 0.5 / stateTexRes;
+  float stateRowIndex = floor((stateIndex + eps) / stateTexRes);
+  vec2 stateTexIndex = vec2(
+    (stateIndex / stateTexRes) - stateRowIndex + eps,
+    stateRowIndex / stateTexRes
+  );
+
+  vec4 state = texture2D(stateTex, stateTexIndex);
+
+  gl_Position = projection * view * model * vec4(state.x, state.y, 0.0, 1.0);
+
+  // Determine color index
+  float colorIndexCat = state.z * isColoredByCategory;
+  float colorIndexVal = floor(state.w * maxColor) * isColoredByValue;
+  float colorIndex = colorIndexCat + colorIndexVal;
+  // Multiply by the number of color states per color
+  // I.e., normal, active, hover, background, etc.
+  colorIndex *= numColorStates;
+  // Half a "pixel" or "texel" in texture coordinates
+  eps = 0.5 / colorTexRes;
+  float colorLinearIndex = colorIndex + globalState;
+  // Need to add cEps here to avoid floating point issue that can lead to
+  // dramatic changes in which color is loaded as floor(3/2.9) = 1 but
+  // floor(3/3.0001) = 0!
+  float colorRowIndex = floor((colorLinearIndex + eps) / colorTexRes);
+
+  vec2 colorTexIndex = vec2(
+    (colorLinearIndex / colorTexRes) - colorRowIndex + eps,
+    colorRowIndex / colorTexRes
+  );
+
+  color = texture2D(colorTex, colorTexIndex);
+
+  // The final scaling consists of linear scaling in [0, 1] and log scaling
+  // in [1, [
+  float finalScaling = min(1.0, scaling) + log2(max(1.0, scaling));
+
+  gl_PointSize = pointSize * finalScaling + pointSizeExtra;
+}
+`;
 
 import {
   COLOR_ACTIVE_IDX,
